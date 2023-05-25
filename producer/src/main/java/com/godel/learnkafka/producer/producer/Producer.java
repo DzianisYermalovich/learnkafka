cf403e2f2011214838558491120b3e4373af3dac
Dzianis Yermalovich <d.yermalovich@godeltech.com>
2023-05-25 16:45:40 +0300

    Sypport key in producer

--- producer/src/main/java/com/godel/learnkafka/producer/producer/Producer.java
+++ producer/src/main/java/com/godel/learnkafka/producer/producer/Producer.java
@@ -12,7 +12,11 @@ public class Producer {
     private final KafkaTemplate<Object, Object> kafkaTemplate;
 
     public void send(final Topic topic, final Object body) {
-        kafkaTemplate.send(topic.getName(), body);
+        send(topic, null, body);
+    }
+
+    public void send(final Topic topic, final Object key, final Object body) {
+        kafkaTemplate.send(topic.getName(), key, body);
     }
 
 }
