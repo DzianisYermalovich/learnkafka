ecb53a5c8d4776b5fda3e7bbdb12fb5ada979419
Dzianis Yermalovich <d.yermalovich@godeltech.com>
2023-05-25 18:06:47 +0300

    Refactor TransactionIntegrationTest

--- producer/src/test/java/com/godel/learnkafka/producer/transaction/TransactionIntegrationTest.java
+++ producer/src/test/java/com/godel/learnkafka/producer/transaction/TransactionIntegrationTest.java
@@ -1,31 +1,27 @@
 package com.godel.learnkafka.producer.transaction;
 
+import com.godel.learnkafka.producer.topic.Topic;
 import org.apache.kafka.clients.consumer.Consumer;
-import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
-import org.springframework.kafka.test.utils.KafkaTestUtils;
 
 import java.time.LocalDateTime;
-import java.util.Map;
 
-import static com.godel.learnkafka.producer.topic.Topic.TopicNames.TRANSACTION;
+import static com.godel.learnkafka.producer.consumer.TestConsumerFactory.createConsumerForTopic;
+import static com.godel.learnkafka.producer.consumer.TestConsumerUtils.getRecord;
+import static com.godel.learnkafka.producer.topic.Topic.TRANSACTION;
 import static com.godel.learnkafka.producer.transaction.OrderType.INCOME;
-import static org.apache.kafka.clients.consumer.ConsumerConfig.AUTO_OFFSET_RESET_CONFIG;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.springframework.kafka.support.serializer.JsonDeserializer.TRUSTED_PACKAGES;
-import static org.springframework.kafka.test.utils.KafkaTestUtils.getRecords;
 
 @SpringBootTest(webEnvironment= RANDOM_PORT)
 @EmbeddedKafka(
-        topics = {TRANSACTION},
+        topics = {Topic.TopicNames.TRANSACTION},
         bootstrapServersProperty = "spring.kafka.bootstrap-servers")
 class TransactionIntegrationTest {
 
@@ -36,11 +32,15 @@ class TransactionIntegrationTest {
     final static Double PRICE = 9.99;
     final static LocalDateTime CREATED_AT = LocalDateTime.now();
 
+    private static Consumer<Long, Transaction> consumer;
+
     @Autowired
     private TestRestTemplate testRestTemplate;
 
-    @Autowired
-    private EmbeddedKafkaBroker embeddedKafka;
+    @BeforeAll
+    static void setup(@Autowired final EmbeddedKafkaBroker embeddedKafka) {
+        consumer = createConsumerForTopic(embeddedKafka, TRANSACTION, Transaction.class);
+    }
 
     @Test
     void shouldAddTransactionDataIntoKafka() {
@@ -50,34 +50,9 @@ class TransactionIntegrationTest {
                 .getStatusCode()
                 .is2xxSuccessful();
 
-        final var actualTransaction = getActualTransaction();
+        final var actualTransactionRecord = getRecord(consumer);
+        final var actualTransaction = actualTransactionRecord.value();
         assertEquals(givenTransaction, actualTransaction);
-    }
-
-    private Transaction getActualTransaction() {
-        final var consumer = getConsumer();
-        final var records = getRecords(consumer);
-        return records.iterator().next().value();
-    }
-
-    private Consumer<Object, Transaction> getConsumer() {
-        final var consumerFactory = getConsumerFactory();
-        final var consumer = consumerFactory.createConsumer();
-        embeddedKafka.consumeFromAnEmbeddedTopic(consumer, TRANSACTION);
-        return consumer;
-    }
-
-    private DefaultKafkaConsumerFactory<Object, Transaction> getConsumerFactory() {
-        final var consumerProps = getConsumerProps();
-        return  new DefaultKafkaConsumerFactory<>(consumerProps, null, new JsonDeserializer<>());
-    }
-
-    @NotNull
-    private Map<String, Object> getConsumerProps() {
-        final var consumerProps = KafkaTestUtils.consumerProps("testGroup", "true", embeddedKafka);
-        consumerProps.put(AUTO_OFFSET_RESET_CONFIG, "earliest");
-        consumerProps.put(TRUSTED_PACKAGES, "com.godel.learnkafka.*");
-        return consumerProps;
     }
 
 }
