94a3ed73df1b795426620df030a653865d760edf
Dzianis Yermalovich <d.yermalovich@godeltech.com>
2023-05-30 17:10:07 +0300

    Use singleton containers

--- consumer/src/test/java/com/godel/learnkafka/consumer/base/BaseIntegrationTest.java
+++ consumer/src/test/java/com/godel/learnkafka/consumer/base/BaseIntegrationTest.java
@@ -9,7 +9,6 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
@@ -24,19 +23,23 @@ public abstract class BaseIntegrationTest {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @Container
-    static KafkaContainer kafka = new KafkaContainer(parse("confluentinc/cp-kafka:6.2.1"));
+    static final KafkaContainer KAFKA_CONTAINER;
+    static final PostgreSQLContainer<?> POSTGRE_CONTAINER;
 
-    @Container
-    static PostgreSQLContainer<?> postgre = new PostgreSQLContainer<>(parse("postgres:15.3"));
+    static {
+        KAFKA_CONTAINER = new KafkaContainer(parse("confluentinc/cp-kafka:6.2.1"));
+        KAFKA_CONTAINER.start();
+        POSTGRE_CONTAINER = new PostgreSQLContainer<>(parse("postgres:15.3"));
+        POSTGRE_CONTAINER.start();
+    }
 
 
     @DynamicPropertySource
     static void setupProperties(final DynamicPropertyRegistry registry) {
-        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
-        registry.add("spring.datasource.url", postgre::getJdbcUrl);
-        registry.add("spring.datasource.username", postgre::getUsername);
-        registry.add("spring.datasource.password", postgre::getPassword);
+        registry.add("spring.kafka.bootstrap-servers", KAFKA_CONTAINER::getBootstrapServers);
+        registry.add("spring.datasource.url", POSTGRE_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRE_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", POSTGRE_CONTAINER::getPassword);
     }
 
     @AfterEach
